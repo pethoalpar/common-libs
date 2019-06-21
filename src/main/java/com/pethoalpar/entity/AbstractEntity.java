cdf603e9d365181e4de964b3b2e165c66867e7ae
pethoalpar <pethoalpar@gmail.com>
2019-06-21 20:58:53 +0300

    Add audit fields

--- src/main/java/com/pethoalpar/entity/AbstractEntity.java
+++ src/main/java/com/pethoalpar/entity/AbstractEntity.java
@@ -1,10 +1,19 @@
 package com.pethoalpar.entity;
 
+import java.util.Date;
+
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -14,6 +23,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 @MappedSuperclass
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractEntity {
 
 	@Id
@@ -21,12 +31,60 @@ public abstract class AbstractEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 
+	@Column(updatable = false)
+	@CreatedDate
+	protected Date createdAt;
+
+	@Column
+	@CreatedBy
+	protected String createdBy;
+
+	@Column
+	@LastModifiedDate
+	protected Date modifiedAt;
+
+	@Column
+	@LastModifiedBy
+	protected String modifiedBy;
+
 	public Integer getId() {
 		return this.id;
 	}
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return this.createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedBy() {
+		return this.createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getModifiedAt() {
+		return this.modifiedAt;
+	}
+
+	public void setModifiedAt(Date modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
+	public String getModifiedBy() {
+		return this.modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
 	}
 
 }
